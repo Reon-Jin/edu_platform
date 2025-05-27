03ebf392e1826e26b9329fbc43d24afd503b1e35
Reon-Jin <Reon_Jin@outlook.com>
2025-05-27 19:58:35 +0800

    注册登录页面的BE部分
    注册登录页面的BE部分
    
    Co-Authored-By: Always243640 <213021189+Always243640@users.noreply.github.com>
    Co-Authored-By: Zilin Wang <148900012+zilinwang617@users.noreply.github.com>

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
+# backend/main.py
 from fastapi import FastAPI
+from backend.auth import router as auth_router
 
 app = FastAPI()
+app.include_router(auth_router)
 
 @app.get("/")
 async def root():
-    return {"msg": "Hello FastAPI"}
+    return {"msg": "OK"}
