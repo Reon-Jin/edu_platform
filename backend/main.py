366034d2e2992e0cce82f9a42d2247cad49d95df
Reon-Jin <Reon_Jin@outlook.com>
2025-05-27 20:32:53 +0800

    Revert "注册登录页面的BE部分"

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,7 @@
-# backend/main.py
 from fastapi import FastAPI
-from backend.auth import router as auth_router
 
 app = FastAPI()
-app.include_router(auth_router)
 
 @app.get("/")
 async def root():
-    return {"msg": "OK"}
+    return {"msg": "Hello FastAPI"}
